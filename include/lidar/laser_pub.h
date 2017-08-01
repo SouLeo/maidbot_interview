88c2150a43e79692e81f901925368bed13aa6910
selma <slwanna@utexas.edu>
2017-08-01 14:37:45 -0600

    first attempt complete

--- include/lidar/laser_pub.h
+++ include/lidar/laser_pub.h
@@ -8,26 +8,25 @@ class LaserScan {
     private:
 		unsigned int num_readings;
 		double laser_frequency;
-        int count; 
-		ros::Rate r;
-
-		ros::NodeHandle n;
-		ros::Publisher laser_scan_pub;
+        ros::NodeHandle n;
 
 	public:
-		// member access
+		// ROS
+       	ros::Publisher laser_scan_pub;
+        
+        // Filling Scan Messages
         sensor_msgs::LaserScan scan;
 	    sensor_msgs::LaserScan * fill_scan(sensor_msgs::LaserScan& scan, ros::Time scan_time, std::string frame_id, float ang_min, float ang_max, float ang_incr, double time_incr, float range_min, float range_max, unsigned int num_readings);
         
+        // Member access
         std::vector<double> ranges;
 		std::vector<double> intensities;
 
 		unsigned int get_num_readings();
 		double get_laser_frequency();
 		int get_count();
-
-		// constructor/destructor
 		
+        // constructor/destructor
 		LaserScan();
 		~LaserScan();
 
--- src/laser_pub.cpp
+++ src/laser_pub.cpp
@@ -1,20 +1,15 @@
-// Author: Selma Wanna
-// Reference Material: 
+// Author: Selma Wanna Reference Material:
 // http://wiki.ros.org/navigation/Tutorials/RobotSetup/Sensors#Writing_Code_to_Publish_a_LaserScan_Message
 // http://wiki.ros.org/laser_pipeline/Tutorials/IntroductionToWorkingWithLaserScannerData
 
 #include "lidar/laser_pub.h"
 
-LaserScan::LaserScan() :
-    n("~"),
+LaserScan::LaserScan() : n("~"),
 
-    num_readings(100),
-    laser_frequency(40),
-    count(0),
-    r(1.0)
+    num_readings(100), laser_frequency(40)
 
-{
-    ros::Publisher laser_scan_pub = n.advertise<sensor_msgs::LaserScan>("sensor_msgs/LaserScan",50);
+{ ros::Publisher laser_scan_pub =
+    n.advertise<sensor_msgs::LaserScan>("sensor_msgs/LaserScan",50);
 
 }
 
@@ -24,39 +19,59 @@ unsigned int LaserScan::get_num_readings(void) { return this->num_readings; }
 
 double LaserScan::get_laser_frequency(void) { return this->laser_frequency; }
 
-int LaserScan::get_count(void) { return this->count; }
-
-// Fill function
-void fill_scan(sensor_msgs::LaserScan& scan, ros::Time scan_time, std::string frame_id, float ang_min, float ang_max, float ang_incr, double time_incr, float range_min, float range_max, unsigned int num_readings){
-    scan.header.stamp = scan_time;   
+// Fill Laser Scan Message function
+void fill_scan(sensor_msgs::LaserScan& scan, std::string
+        frame_id, float ang_min, float ang_max, float ang_incr, double
+        time_incr, float range_min, float range_max, unsigned int num_readings){
+    scan.header.frame_id = frame_id;
+    scan.angle_min = ang_min;
+    scan.angle_max = ang_max;
+    scan.angle_increment = ang_incr;
+    scan.time_increment = time_incr;
+    scan.range_min = range_min;
+    scan.range_max = range_max;
+    scan.ranges.resize(num_readings);
+    scan.intensities.resize(num_readings);
 }
 
-
 int main(int argc, char **argv) {
-	// ROS setup
+    unsigned int count(0);
+    // ROS setup
+    ros::Rate r(1.0);
     ros::init(argc, argv, "laser_pub");
 	
-	//create instance of LaserScan
+    //create instance of LaserScan
     LaserScan* laser_pub_node = new LaserScan();
 
+    float ang_incr = 3.14 / laser_pub_node->get_num_readings(); 
+    double time_incr = (1 / laser_pub_node->get_laser_frequency()) /
+        laser_pub_node->get_num_readings();
+
+    fill_scan(laser_pub_node->scan, "laser_frame", -1.57, 1.57,
+            ang_incr, time_incr, 0.00, 100.0,
+            laser_pub_node->get_num_readings());
+    
     while(ros::ok()){
-    // generating fake data for laser scan
-		
+    
+        // generating fake data for laser scan
         for (unsigned int i = 0; i < laser_pub_node->get_num_readings(); i++){
-            laser_pub_node->ranges.push_back(laser_pub_node->get_count());
-            laser_pub_node->intensities.push_back(100 + laser_pub_node->get_count());
-        }
-        ros::Time scan_time = ros::Time::now();
+            laser_pub_node->ranges.push_back(count);
+            laser_pub_node->intensities.push_back(100 + count); 
+        } 
     
         //populate LaserScan message
-        float ang_incr = 3.14 / laser_pub_node->get_num_readings();
-        double time_incr = (1 / laser_pub_node->get_laser_frequency()) / laser_pub_node->get_num_readings();
-        fill_scan(laser_pub_node->scan, scan_time, "laser_frame", -1.57, 1.57, ang_incr, time_incr, 0.00, 100.0, laser_pub_node->get_num_readings());
+        ros::Time scan_time = ros::Time::now();
+        laser_pub_node->scan.header.stamp = scan_time; 
+        for(unsigned int i = 0; i < laser_pub_node->get_num_readings(); i++){
+            laser_pub_node->scan.ranges[i] = laser_pub_node->ranges[i];
+            laser_pub_node->scan.intensities[i] = laser_pub_node->intensities[i];
+        }
+        laser_pub_node->laser_scan_pub.publish(laser_pub_node->scan); 
+        count++;
+        r.sleep();
 
-    }
-	
+    }	
 
     ros::waitForShutdown();
 
-    return 0;
-}
+    return 0; }
