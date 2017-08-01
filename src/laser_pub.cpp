6abfd8069dcd0186bc0b35ad20c6e633fdcc77eb
selma <slwanna@utexas.edu>
2017-08-01 12:05:35 -0600

    fix spacing?

--- src/laser_pub.cpp
+++ src/laser_pub.cpp
@@ -5,21 +5,23 @@
 #include "lidar/laser_pub.h"
 
 LaserScan::LaserScan() :
-	n("~"),
+    n("~"),
 
-	num_readings(100),
+    num_readings(100),
     laser_frequency(40),
-	count(0),
-	r(1.0)
-
+    count(0),
+    r(1.0)
 
 {
 	ros::Publisher laser_scan_pub = n.advertise<sensor_msgs::LaserScan>("sensor_msgs/LaserScan",50);
 
 }
 
+// Getters for private member variables
 unsigned int LaserScan::get_num_readings(void) { return this->num_readings; }
+
 double LaserScan::get_laser_frequency(void) { return this->laser_frequency; }
+
 int LaserScan::get_count(void) { return this->count; }
 
 int main(int argc, char **argv) {
@@ -34,7 +36,7 @@ int main(int argc, char **argv) {
 		
 		for (unsigned int i = 0; i < laser_pub_node->get_num_readings(); i++){
 		    laser_pub_node->ranges.push_back(laser_pub_node->get_count());
-			laser_pub_node->intensities.push_back(100 + laser_pub_node->get_count());
+            laser_pub_node->intensities.push_back(100 + laser_pub_node->get_count());
 		}
 		ros::Time scan_time = ros::Time::now();
 		
