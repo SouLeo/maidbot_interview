e926201a6f499b80bf2eab7c8244f2a239a87e01
selma <slwanna@utexas.edu>
2017-08-07 17:21:25 -0600

    it builds

--- src/fake_odom/src/odom_pub.cpp
+++ src/fake_odom/src/odom_pub.cpp
@@ -33,6 +33,11 @@ void FakeOdom::odom_trans_update(geometry_msgs::TransformStamped& foo, ros::Time
     foo.transform.rotation = odom_quat;
 }
 
+void odom_init(nav_msgs::Odometry& odom){
+    odom.header.frame_id = "odom";
+    odom.child_frame_id = "base_link";
+    odom.pose.pose.position.z = 0.0;
+}
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "odom_pub");
@@ -46,6 +51,9 @@ int main(int argc, char** argv) {
     ros::Rate r(1.0);
 
     odom_node->odom_trans_init(odom_node->odom_trans, current_time);
+
+    nav_msgs::Odometry odom;
+    odom_init(odom);
 
     while(ros::ok()) {
         ros::spinOnce();
@@ -62,30 +70,27 @@ int main(int argc, char** argv) {
         odom_node->th +=  odom_node->delta_th;
 
         odom_node->odom_quat = tf::createQuaternionMsgFromYaw(odom_node->th);
-        odom_node->odom_trans_update(odom_node->odom_trans, current_time, x, y, odom_quat);
+        odom_node->odom_trans_update(odom_node->odom_trans, current_time, odom_node->x, odom_node->y, odom_node->odom_quat);
         
         // send the transform
-        odom_node->odom_broadcaster.sendTransform(odom_trans);
+        odom_node->odom_broadcaster.sendTransform(odom_node->odom_trans);
 
         // pub the odom message
         nav_msgs::Odometry odom;
         odom.header.stamp = current_time;
-        odom.header.frame_id = "odom";
 
         // set the position
-        odom.pose.pose.position.x = x;
-        odom.pose.pose.position.y = y;
-        odom.pose.pose.position.z = 0.0;
-        odom.pose.pose.orientation = odom_quat;
+        odom.pose.pose.position.x = odom_node->x;
+        odom.pose.pose.position.y = odom_node->y;
+        odom.pose.pose.orientation = odom_node->odom_quat;
 
         // set the velocity
-        odom.child_frame_id = "base_link";
-        odom.twist.twist.linear.x = vx;
-        odom.twist.twist.linear.y = vy;
-        odom.twist.twist.angular.z = vth;
+        odom.twist.twist.linear.x = odom_node->getvx();
+        odom.twist.twist.linear.y = odom_node->getvy();
+        odom.twist.twist.angular.z = odom_node->getvth();
 
         // publish the message
-        odom_pub.publish(odom);
+        odom_node->odom_pub.publish(odom);
 
         last_time = current_time;
         r.sleep();
